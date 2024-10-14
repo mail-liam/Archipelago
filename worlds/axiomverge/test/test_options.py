86f83b79c74b42d23b0a94f3b2fe09b7aaa030f5
mail-liam <30469495+mail-liam@users.noreply.github.com>
2024-10-14 20:13:07 +1100

    Set up Progressive Coat test

---
+++ worlds/axiomverge/test/test_options.py
@@ -0,0 +1,27 @@
+from . import AVTestBase
+
+
+class TestOptionProgCoatOff(AVTestBase):
+    options = {
+        "Progressive Drone": "false",
+    }
+
+    def test_prog_coat_off(self):
+        prog_coats = self.get_items_by_name(("Progressive Coat",))
+        self.assertListEqual(prog_coats, [])
+
+        base_coats = self.get_items_by_name(("Modified Lab Coat", "Trenchcoat", "Red Coat"))
+        self.assertListEqual([coat.name for coat in base_coats], ["Modified Lab Coat", "Trenchcoat", "Red Coat"])
+
+
+class TestOptionProgCoatOff(AVTestBase):
+    options = {
+        "Progressive Drone": "true",
+    }
+
+    def test_prog_coat_on(self):
+        base_coats = self.get_items_by_name(("Modified Lab Coat", "Trenchcoat", "Red Coat"))
+        self.assertListEqual(base_coats, [])
+
+        prog_coats = self.get_items_by_name(("Progressive Coat",))
+        self.assertEqual(len(prog_coats), 3)
