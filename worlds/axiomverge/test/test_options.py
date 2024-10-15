1ac6cff02c3dc1ce8e007ac92e3f35daf4f6c24a
mail-liam <30469495+mail-liam@users.noreply.github.com>
2024-10-15 12:50:33 +1100

    Progressive Disruptor tests

--- worlds/axiomverge/test/test_options.py
+++ worlds/axiomverge/test/test_options.py
@@ -7,11 +7,11 @@ class TestOptionProgCoatOff(AVTestBase):
     }
 
     def test_prog_coat_off(self):
-        prog_coats = self.get_items_by_name(("Progressive Coat",))
-        self.assertListEqual(prog_coats, [])
+        prog_items = self.get_items_by_name(("Progressive Coat",))
+        self.assertListEqual(prog_items, [])
 
-        base_coats = self.get_items_by_name(("Modified Lab Coat", "Trenchcoat", "Red Coat"))
-        self.assertListEqual([coat.name for coat in base_coats], ["Modified Lab Coat", "Trenchcoat", "Red Coat"])
+        base_items = self.get_items_by_name(("Modified Lab Coat", "Trenchcoat", "Red Coat"))
+        self.assertListEqual([item.name for item in base_items], ["Modified Lab Coat", "Trenchcoat", "Red Coat"])
 
 
 class TestOptionProgCoatOn(AVTestBase):
@@ -20,8 +20,34 @@ class TestOptionProgCoatOn(AVTestBase):
     }
 
     def test_prog_coat_on(self):
-        base_coats = self.get_items_by_name(("Modified Lab Coat", "Trenchcoat", "Red Coat"))
-        self.assertListEqual(base_coats, [])
+        base_items = self.get_items_by_name(("Modified Lab Coat", "Trenchcoat", "Red Coat"))
+        self.assertListEqual(base_items, [])
 
-        prog_coats = self.get_items_by_name(("Progressive Coat",))
-        self.assertEqual(len(prog_coats), 3)
+        prog_items = self.get_items_by_name(("Progressive Coat",))
+        self.assertEqual(len(prog_items), 3)
+
+
+class TestOptionProgCoatOff(AVTestBase):
+    options = {
+        "progressive_address_disruptor": 0,
+    }
+
+    def test_prog_disruptor_off(self):
+        prog_items = self.get_items_by_name(("Progressive Address Disruptor",))
+        self.assertListEqual(prog_items, [])
+
+        base_items = self.get_items_by_name(("Address Disruptor 1", "Address Disruptor 2"))
+        self.assertListEqual([item.name for item in base_items], ["Address Disruptor 1", "Address Disruptor 2"])
+
+
+class TestOptionProgDisruptorOn(AVTestBase):
+    options = {
+        "progressive_address_disruptor": 1,
+    }
+
+    def test_prog_disruptor_on(self):
+        base_items = self.get_items_by_name(("Address Disruptor 1", "Address Disruptor 2"))
+        self.assertListEqual(base_items, [])
+
+        prog_items = self.get_items_by_name(("Progressive Address Disruptor",))
+        self.assertEqual(len(prog_items), 2)
