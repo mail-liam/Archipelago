1e41ed5f8bcef60dfc1da213e1071746e5bb505a
mail-liam <30469495+mail-liam@users.noreply.github.com>
2025-04-18 18:08:33 +1000

    Add the option

--- worlds/axiomverge/options.py
+++ worlds/axiomverge/options.py
@@ -106,6 +106,12 @@ class RequireNodes(DefaultOnToggle):
     """
 
 
+class GlitchSanity(Toggle):
+    """
+    Glitching enemies provides checks. Adds 58 locations to the pool.
+    """
+
+
 @dataclass
 class AxiomVergeOptions(PerGameCommonOptions):
     allow_displacement_warps: AllowDisplacementWarps
@@ -121,4 +127,5 @@ class AxiomVergeOptions(PerGameCommonOptions):
     progressive_drone: ProgressiveDrone
     secret_world_weapons: ShuffleSecretWorldWeapons
     require_nodes: RequireNodes
+    glitchsanity: GlitchSanity
     start_location: StartLocation
