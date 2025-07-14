5d512e7027ce8df4b828064be026fb4c4627de97
mail-liam <30469495+mail-liam@users.noreply.github.com>
2025-07-14 13:52:31 +1000

    Add boss data file

---
+++ worlds/axiomverge/boss_data.py
@@ -0,0 +1,12 @@
+from .constants import AVRegion
+
+BOSS_DATA = (
+    ("Xedur", AVRegion.XEDUR),
+    ("Telal", AVRegion.LOWER_ABSU),
+    ("Uruku", AVRegion.URUKU),
+    ("Gir-Tab", AVRegion.GIR_TAB_LOWER_ENTRANCE),
+    ("Clone", AVRegion.CLONE),
+    ("Ukhu", AVRegion.UKHU),
+    ("Sentinel", AVRegion.MAR_URU_ENTRANCE),
+    ("Xedur Hul", AVRegion.POST_SENTINEL),
+)
